34517fa4f72b2792b806b9efecf9a944f7a1bfa0
AliOlliek <olliek95@gmail.com>
2022-05-27 11:09:11 +0300

    Created Note Object

---
+++ Index.js
@@ -0,0 +1,12 @@
+const note = class {
+    constructor (Id, Title, Description, Point, IsDone, CreatedAt) {
+        this.id = Id;
+        this.title = Title;
+        this.description = Description;
+        this.point = Point;
+        this.Isdone = IsDone;
+        this.CreatedAt = CreatedAt;
+    }
+}
+
+var container = $("#container");
