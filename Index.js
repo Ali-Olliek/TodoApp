a186fd4d0a92887b0d826be4a8b348c1c9192854
AliOlliek <olliek95@gmail.com>
2022-05-28 19:44:14 +0300

    Can Add and remove notes

--- Index.js
+++ Index.js
@@ -9,3 +9,20 @@ const note = class {
     }
 }
 
+let add_note = $("#addnote");
+let note_container = $("#container");
+let note_input = $("#inputfield")
+
+$("#addnote").click(function(){
+  var written_note = document.createElement("p");
+  written_note.classList.add("note");
+  written_note.innerText = $("#inputfield").val(); //https://stackoverflow.com/a/31080986
+  note_container.append(written_note);
+  $("#inputfield").val("")
+  $(written_note).click(function(){
+    $(written_note).css("text-decoration","line-through")
+  })
+  $(written_note).dblclick(function(){
+    $(written_note).remove()
+  })
+})
