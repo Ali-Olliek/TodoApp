ef1f5267326fe19ca958a3a9a655bb9f3f2077f6
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-29 16:55:26 +0300

    removing one note doesn't remove all

--- Index.js
+++ Index.js
@@ -14,31 +14,25 @@ get_random_id = () => {
 }
 
 let add_note = $("#addnote");
-let note_container = $("#container");
+let note_list = $("#notelist");
 let note_input = $("#inputfield");
 
 
 $("#addnote").click(function() {
-  let created_note = new note();
-  let title = created_note.title;
-  let desc = created_note.description;
-  text_to_add = $("#container").append("<p>");
-  $("p").addClass("note")
-  $("p").append(desc);
+  var created_note = new note();
+  text_to_add = $("#container").append("<li>");
+  $("li").prev("li").append(created_note.description);
+  $("li").addClass("note")
   $("#inputfield").val("");
   $("#title").val("")
   let note_list = [];
   note_list.push(created_note);
   localStorage.setItem("My Notes", JSON.stringify(note_list));
-document.querySelectorAll(".note").forEach((item) => {
-  item.addEventListener("click", (event) => {
-    $("p").css("text-decoration", "line-through");
-  });
-});
+  $(".note").on("click", function(){
+    $(this).closest("p").css("text-decoration", "line-through");
+  })
+  $(".note").on("dblclick", function(){
+    $(this).closest("p").remove()
+  })
 
-document.querySelectorAll(".note").forEach((item) => {
-  item.addEventListener("dblclick", (event) => {
-    $("p").remove();
-  });
-});
 })
