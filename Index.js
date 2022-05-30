f5b769aeeef9a6561bfc290796b5bc7eff2b8155
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-30 09:12:04 +0300

    Fixed note marking and deleting

--- Index.js
+++ Index.js
@@ -21,7 +21,7 @@ $("#addnote").click(function() {
   var created_note = new note();
   let text_to_add = created_note.description
   text_to_add = $("#notelist").append("<li>");
-  $("#container ul li:last").append(note_input.val());
+  $("#container ul li:last").append(note_input.val()); //https://stackoverflow.com/q/1145208/18590539
   $("li").addClass("note")
   
   // reset fields
@@ -31,14 +31,14 @@ $("#addnote").click(function() {
   // create JSON object
   let note_list = [];
   note_list.push(created_note);
-
-  // delete and mark note as done
   localStorage.setItem("My Notes", JSON.stringify(note_list));
+  
+  // delete and mark note as done
   $(".note").on("click", function(){
-    $(this).closest("p").css("text-decoration", "line-through");
+    $(this).closest("li").css("text-decoration", "line-through");
   })
   $(".note").on("dblclick", function(){
-    $(this).closest("p").remove()
+    $(this).closest("li").remove("li")
   })
 
 })
