3df191f133a11f4c23127572bec92de3da957be7
AliOlliek <olliek95@gmail.com>
2022-06-02 18:52:56 +0300

    Local Storage and Newly Added Notes work fine

--- Index.js
+++ Index.js
@@ -1,4 +1,7 @@
+// ---------------------HEADER-------------------------//
+
 // Display week day, quote of the day and author in header
+
 let quotes = ['"It always seems impossible until it is done"','"Start where you are. Use what you have. Do What you can"','"When something is important enough, you do it even if the odds are not in your favor"','"If you are going through hell, keep going"','"It does not matter how slowly you go as long as you do not stop"','"Veni vidi vici"','"If you can dream it, you can do it"'];
 let authors = ["Nelson Mandela","Arthur Ashe","Elon Musk","Winston Churchill","Confucius","Julius Ceasar","Walt Disney"]
 let weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
@@ -14,7 +17,7 @@ $("#author").html(quote_author);
 $("#quote").html(quote_of_the_day);
 $("#day").html(day)
 
-//--------------------------//
+//-----------------------Class Construction-------------------------------- //
 const note = class {
     constructor (Id, Title, Description, Point, IsDone, CreatedAt) {
       this.id = get_random_id();
@@ -30,65 +33,83 @@ let mil_in_year = 3.154e+10;
 get_random_id = () => {
    return Math.floor(Math.random() * 1000);
 }
+
+// -----------------------Main------------------------------ //
+
+let create_main_list = function(){
+  var main_list = JSON.parse(localStorage.getItem("My Notes"));
+  return main_list
+}
+
 // On Load
 
 $(document).ready(function () {
-  var loaded_notes = JSON.parse(localStorage.getItem("My Notes"));
-  for (let i = 0; i < loaded_notes.length; i++) {
-    let text_to_add = loaded_notes[i].description;
-    console.log(text_to_add);
-    $("#notelist").append("<li>");
-    $("#container ul li:last").append(text_to_add); //https://stackoverflow.com/q/1145208/18590539
-    $("li").addClass("note");
+  main_list = create_main_list()
+  console.log(main_list)
+  if(main_list != null){
+    for(let i = 0; i<main_list.length;i++){
+      let text_to_add = main_list[i].description;
+      $("#notelist").append("<li>");
+      $("#container ul li:last").append(text_to_add); //https://stackoverflow.com/q/1145208/18590539
+      $("li").addClass("note");
   }
+  }else{
+  return main_list = []
+}
+});
 
   let add_note = $("#addnote");
   let note_list = $("#notelist");
   let note_input = $("#inputfield");
   let list_of_notes = [];
 
-  $("#addnote").click(function () {
-    // DueDate
-    let time_due = $("#date").val();
-    console.log(time_due);
-    if (time_due - Date.now < 8.64e7) {
-      $("body").css("background-color", "red");
-    }
+$("#addnote").click(function () {
+  main_list = create_main_list()
+  let new_list = []; // list of notes we are going to add
+  var created_note = new note();
+  let text_to_add = created_note.description;
+  new_list.push(created_note)
+  text_to_add = $("#notelist").append("<li>");
+  $("#container ul li:last").append(note_input.val()); //https://stackoverflow.com/q/1145208/18590539
+  $("li").addClass("note");
+  $("#inputfield").val("");
+  $("#title").val("");
 
-    var created_note = new note();
-    let text_to_add = created_note.description;
-    text_to_add = $("#notelist").append("<li>");
-    $("#container ul li:last").append(note_input.val()); //https://stackoverflow.com/q/1145208/18590539
-    $("li").addClass("note");
-
-    // reset fields
-    $("#inputfield").val("");
-    $("#title").val("");
-
-    // Create List of notes
-    list_of_notes.push(created_note);
-
-    // create JSON object
-    note_list.push(created_note);
+// Create and merge List of notes and convert to JSON
+  if(main_list == null){
+    list_of_notes=new_list;
     localStorage.setItem("My Notes", JSON.stringify(list_of_notes));
-
-    // default sort by time created
-    list_of_notes.sort((note1, note2) => {
-      if (note1.point > note2.point) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-    $("#notelist").html("");
-    for (let i = 0; i < list_of_notes.length; i++) {
-      text = list_of_notes[i].description;
-      sorted_notes = $("#notelist").append("<li>");
-      $("#container ul li:last").append(text);
-      $("li").addClass("note");
-    }
-  });
+  }else{
+    list_of_notes = new_list.concat(main_list);
+    localStorage.setItem("My Notes", JSON.stringify(list_of_notes));
+  }
+  
+// default sort by time created
+list_of_notes.sort((note1, note2) => {
+  if (note1.point > note2.point) {
+    return 1;
+  } else {
+    return -1;
+  }
 });
+$("#notelist").html("");
+for (let i = 0; i < list_of_notes.length; i++) {
+  text = list_of_notes[i].description;
+  sorted_notes = $("#notelist").append("<li>");
+  $("#container ul li:last").append(text);
+  $("li").addClass("note");
+}
+});
+
+// DueDate
+let time_due = $("#date").val();
+if (time_due - Date.now < 8.64e7) {
+  $("body").css("background-color", "red");
+}
+
+
+  
+
 // search based on title or description 
 
 $('#search').keypress(function(event){
@@ -99,21 +120,19 @@ $('#search').keypress(function(event){
       text_to_search = $("#search").val()
       let found = Object.values(list_of_notes[i]).includes(text_to_search);
 
-      // store the found values in an array
-      if(found){
-        found_notes.push(list_of_notes[i])
-
-      // display the found notes
-
+    // store the found values in an array
+    if(found){
+      found_notes.push(list_of_notes[i])
       }
-          $("#notelist").html("")
-          for(let j = 0; j<found_notes.length; j++){
-          text = found_notes[j].description;
-          sorted_notes = $("#notelist").append("<li>");
-          $("#container ul li:last").append(text);
-          $("li").addClass("note");
+    // display the found notes
+      $("#notelist").html("")
+      for(let j = 0; j<found_notes.length; j++){
+      text = found_notes[j].description;
+      sorted_notes = $("#notelist").append("<li>");
+      $("#container ul li:last").append(text);
+      $("li").addClass("note");
 
-        }
+    }
     }  
   }
 });
